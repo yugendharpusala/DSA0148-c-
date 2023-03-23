5c16ab24e89a1f88e52115f0506294f1096cc27c
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:36:12 +0530

    Add files via upload

---
+++ assignment3 Q7.cpp
@@ -0,0 +1,15 @@
+Q>>What is the output
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+    if (!(cout << "SIMATS"))
+    cout <<" SIMATS ";
+    else
+    cout << "SCHOOL OF ENGINEERING ";
+     
+    return 0;
+}
+
+SOLUTION>> the above program gives the output as "SIMATS SCHOOL OF ENGINEERING "
