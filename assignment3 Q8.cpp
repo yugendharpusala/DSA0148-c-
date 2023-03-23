538107732c64a87ef6e550bf232913e80003687d
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:36:29 +0530

    Add files via upload

---
+++ assignment3 Q8.cpp
@@ -0,0 +1,13 @@
+Q>Q>>Find the output
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+   int n = 4;
+   n = n >> 2;
+   cout << n;
+   return 0;
+}
+
+SOLUTION>> The output of the above program is "1"
