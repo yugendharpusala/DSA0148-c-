0b45a5c700f3b0f73626c84ed1c5303b0f1d2060
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:34:56 +0530

    Add files via upload

---
+++ assignment3 Q3.cpp
@@ -0,0 +1,16 @@
+Q>>  Find the output of the following program
+#include<iostream>
+ 
+using namespace std;
+int N = 10;
+ 
+int main()
+{
+  static int x = 1;
+  if (cout << x << " " && x++ < N && main())
+  { }
+  return 0;
+}
+
+
+SOLUTION>>The output for the above program is "1 2 3 4 5 6 7 8 9 10"
