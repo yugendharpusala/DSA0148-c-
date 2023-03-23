948c87a983b012183d3f46eafe38b0b729130105
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:34:32 +0530

    Add files via upload

---
+++ assignment3 Q2.cpp
@@ -0,0 +1,18 @@
+Q>>What will be the output of the following program
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int x = 10;
+   int y = 10;
+    
+   if (!(x ^ y))
+      cout << " x is equal to y ";
+   else
+      cout << " x is not equal to y ";
+       
+   return 0;
+}
+
+SOLUTION>>The output for the above program is "x is equal to y"
