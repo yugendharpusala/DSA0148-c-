1c1270cbc88e313028476a151b7268c728784177
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:36:45 +0530

    Add files via upload

---
+++ assignment3 Q9.cpp
@@ -0,0 +1,13 @@
+Q>> Find the output of following code: 
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+   int n = 4;
+   n = n >> 2;
+   cout << n;
+   return 0;
+}
+
+SOLUTION>>the output for the above program is "1"
