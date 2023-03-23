977edba064b7107e4894eb7e5f8f711df43f62ff
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:33:50 +0530

    Add files via upload

---
+++ assignment3 Q1.cpp
@@ -0,0 +1,9 @@
+1. Idnetify the error for the following programs,
+#include<iostream>
+ int main()
+{
+  cout << "\"geeksforgeeks\"";
+   }
+
+SOLUTION:
+  >>The above code is perfectly alright and it doesnot show any error
