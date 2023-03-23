627374c1f5410f3822076df27080cfed50200385
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:35:49 +0530

    Add files via upload

---
+++ assignment3 Q6.cpp
@@ -0,0 +1,14 @@
+Find the output for the program. 
+ #include<iostream>
+ 
+using namespace std;
+int main()
+{
+  int a = 5;
+  int b = 5;
+  int sum = -( -a-b );
+  cout << sum;
+  return 0;
+}
+
+SOLUTION>>the output of the following program is "10"
