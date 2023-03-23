416a41c5519bc6676b7c5a381a827b879c9224b1
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-23 16:35:36 +0530

    Add files via upload

---
+++ assignment3 Q5.cpp
@@ -0,0 +1,12 @@
+Q>>Can we do in C++ with this header file.
+#include<bits/stdc++.h>
+ 
+int main ()
+{
+   int a = 15, b = 20;
+   printf("max = %d\n", ((a + b) + abs(a - b)) / 2);
+   printf("min = %d", ((a + b) - abs(a - b)) / 2);
+   return 0;
+}
+
+SOLUTION>> yes we can use the header file"#include<bits/stdc++.h> ,the program will compile and run without any error
